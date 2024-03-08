bf31fe8be97825cee6b4d5e82d2729700afa1fc0
JuanEspinosa <jespinosalozano@gmail.com>
2024-03-08 12:00:18 -0500

    Middleware added corsheaders

--- comandos/deployment.py
+++ comandos/deployment.py
@@ -21,7 +21,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    #'corsheaders.middleware.CorsMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
 ]
 # Static files config
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
