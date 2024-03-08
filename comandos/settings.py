46daa6e12d19a10199e632a32ce06678415656cb
JuanEspinosa <jespinosalozano@gmail.com>
2024-03-07 22:09:10 -0500

    Added changes to collectstatic files correctly

--- comandos/settings.py
+++ comandos/settings.py
@@ -143,7 +143,7 @@ LOGIN_URL='/admin/login/'
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 STATIC_ROOT = os.path.join(BASE_DIR, 'static/')
-STATIC_URL = '/static/'
+STATIC_URL = 'static/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
