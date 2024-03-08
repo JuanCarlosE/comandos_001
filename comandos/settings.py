4affd7b6c5b370fa1c491b6cbdd5fc4339184301
JuanEspinosa <jespinosalozano@gmail.com>
2024-03-07 21:43:56 -0500

    Added secret key

--- comandos/settings.py
+++ comandos/settings.py
@@ -25,7 +25,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = env('SECRET_KEY')
+SECRET_KEY = 'django-insecure-zzsficui2zq42!gy=3j-9nknu)hxga+v3t-vy3_u6=ed=g9uwc'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
