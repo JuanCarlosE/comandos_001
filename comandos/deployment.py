3af8cd8742d7073ad35ed39ab9152d923925050a
JuanEspinosa <jespinosalozano@gmail.com>
2024-03-08 20:59:47 -0500

    Changed dict to parameter the db data

--- comandos/deployment.py
+++ comandos/deployment.py
@@ -30,7 +30,8 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 # Database connection
 connection_string = os.environ['AZURE_POSTGRESQL_CONNECTIONSTRING']
-parameters = {pair.split('='):pair.split('=')[1]for pair in connection_string.split(' ')}
+parameters = {pair.split('=')[0]: pair.split('=')[1] for pair in connection_string.split(' ')}
+
 
 DATABASES = {
     'default': {
