974e07aba1b235dec63c87cafc67fcf297f603da
JuanEspinosa <jespinosalozano@gmail.com>
2024-03-13 21:30:41 -0500

    Restart project

--- comandos/settings.py
+++ comandos/settings.py
@@ -10,11 +10,8 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 import os
-import environ
 from pathlib import Path
 
-env = environ.Env()
-environ.Env.read_env()
 
 
 # Build paths inside the project like this: BASE_DIR /   'subdir'.
